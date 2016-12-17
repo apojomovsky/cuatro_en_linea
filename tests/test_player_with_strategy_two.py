876c5055a375c208bb6c82896f028476df38e497
Alexis Pojomovsky <apojomovsky@gmail.com>
2016-12-17 16:22:26 -0400

    Modify player tests to check on game board instead of using a match instance

--- tests/test_player_with_strategy_two.py
+++ tests/test_player_with_strategy_two.py
@@ -16,8 +16,10 @@ class TestPlayer(unittest.TestCase):
                     ['blue', 'blue',  'red', 'blue',  'red', 'blue', 'blue'],
                     ['red',  'blue',  'red', 'blue',  'red', 'blue', 'blue'],
                     ['blue',  'red', 'blue',  'red',  'red',  'red',  'red']])
+        self.assertEqual(board.read_entry(5, 7), None)
         self.assertFalse(self.player_blue.is_winner(board))
         self.player_blue.play(board)
+        self.assertEqual(board.read_entry(5, 7), 'blue')
         self.assertTrue(self.player_blue.is_winner(board))
 
 
@@ -29,8 +31,10 @@ class TestPlayer(unittest.TestCase):
                     [None,  'red',   None,   None,  None, None, 'blue'],
                     [None, 'blue',   None, 'blue',  None, None, 'blue'],
                     [None, 'blue', 'blue', 'blue',  None, None,  'red']])
+        self.assertEqual(board.read_entry(1, 1), None)
         self.assertFalse(self.player_blue.is_winner(board))
         self.player_blue.play(board)
+        self.assertEqual(board.read_entry(1, 1), 'blue')
         self.assertTrue(self.player_blue.is_winner(board))
 
 
@@ -42,8 +46,14 @@ class TestPlayer(unittest.TestCase):
                     ['blue', 'blue',  'red',  'red',   'red', 'blue', 'blue'],
                     ['red',  'blue',  'red',  'red',  'blue', 'blue', 'blue'],
                     ['blue',  'red', 'blue',  'blue', 'blue',  'red',  'red']])
+        self.assertEqual(board.read_entry(4, 2), None)
+        self.assertEqual(board.read_entry(5, 3), None)
+        self.assertEqual(board.read_entry(5, 4), None)
         self.assertFalse(self.player_red.is_winner(board))
         self.player_red.play(board)
+        self.assertEqual(board.read_entry(4, 2), 'red')
+        self.assertEqual(board.read_entry(5, 3), None)
+        self.assertEqual(board.read_entry(5, 4), None)
         self.assertFalse(self.player_red.is_winner(board))
 
     def test_player_with_strategy_two_red_not_winning_on_row(self):
@@ -54,6 +64,14 @@ class TestPlayer(unittest.TestCase):
                     [None,  'blue',  'red', 'blue',  'blue', 'red', 'blue'],
                     ['red', 'blue',  'red', 'blue',  'red', 'blue', 'blue'],
                     ['blue', 'red', 'blue',  'red',  'red',  'red',  'red']])
+        self.assertEqual(board.read_entry(3, 1), None)
+        self.assertEqual(board.read_entry(4, 2), None)
+        self.assertEqual(board.read_entry(5, 3), None)
+        self.assertEqual(board.read_entry(5, 7), None)
         self.assertFalse(self.player_red.is_winner(board))
         self.player_red.play(board)
+        self.assertEqual(board.read_entry(3, 1), 'red')
+        self.assertEqual(board.read_entry(4, 2), None)
+        self.assertEqual(board.read_entry(5, 3), None)
+        self.assertEqual(board.read_entry(5, 7), None)
         self.assertFalse(self.player_red.is_winner(board))
