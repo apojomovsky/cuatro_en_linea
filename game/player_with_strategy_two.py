d89285a7b551deb493a9b212f794ae366906162f
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-01-06 20:53:13 -0300

    Removed old player_with_strategy_* files

--- game/player_with_strategy_two.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from game.player import Player
-
-class PlayerWithStrategyTwo(Player):
-    def play(self, board):
-        board.put_chip(board.retrieve_emptiest_column(), self._color)
