25ce684711c2e52972b378f66c408b5051fc22a8
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-01-10 20:30:41 -0300

    Refactored tests for strategy_two

--- tests/test_strategy_two.py
+++ tests/test_strategy_two.py
@@ -6,10 +6,9 @@ from game.strategy_two import StrategyTwo
 
 class TestPlayer(unittest.TestCase):
     def setUp(self):
-        self.player_blue = Player('blue', StrategyTwo())
-        self.player_red = Player('red', StrategyTwo())
+        self.strategy_two = StrategyTwo()
 
-    def test_player_with_strategy_two_on_almost_full_board(self):
+    def test_strategy_two_on_seventh_column(self):
         board = GameBoard.from_matrix([
                     ['red',    None,   None,   None,   None,   None,   None],
                     ['red',   'red', 'blue',  'red',  'red',  'red',   None],
@@ -17,17 +16,9 @@ class TestPlayer(unittest.TestCase):
                     ['blue', 'blue',  'red', 'blue',  'red', 'blue',  'red'],
                     ['red',  'blue',  'red', 'blue',  'red', 'blue', 'blue'],
                     ['blue',  'red', 'blue',  'red',  'red',  'red',  'red']])
-        expected_board = GameBoard.from_matrix([
-                    ['red',    None,   None,   None,   None,   None,   None],
-                    ['red',   'red', 'blue',  'red',  'red',  'red', 'blue'],
-                    ['red',  'blue',  'red',  'red', 'blue', 'blue', 'blue'],
-                    ['blue', 'blue',  'red', 'blue',  'red', 'blue',  'red'],
-                    ['red',  'blue',  'red', 'blue',  'red', 'blue', 'blue'],
-                    ['blue',  'red', 'blue',  'red',  'red',  'red',  'red']])
-        self.player_blue.play(board)
-        self.assertEqual(board, expected_board)
+        self.assertEqual(self.strategy_two.return_column(board), 7)
 
-    def test_player_with_strategy_two_on_semi_empty_board(self):
+    def test_strategy_two_on_sixth_column(self):
         board = GameBoard.from_matrix([
                     [None,    None,   None,   None,  None, None,   None],
                     [None,    None,   None,   None,  None, None,   None],
@@ -35,17 +26,9 @@ class TestPlayer(unittest.TestCase):
                     [None,    None,   None,   None,  None, None,   None],
                     [None,  'blue',   None, 'blue',  None, None, 'blue'],
                     ['red', 'blue', 'blue', 'blue', 'red', None,  'red']])
-        expected_board = GameBoard.from_matrix([
-                    [None,    None,   None,   None,  None, None,    None],
-                    [None,    None,   None,   None,  None, None,    None],
-                    [None,    None,   None,   None,  None, None,    None],
-                    [None,    None,   None,   None,  None, None,    None],
-                    [None,  'blue',   None, 'blue',  None, None,  'blue'],
-                    ['red', 'blue', 'blue', 'blue', 'red', 'blue', 'red']])
-        self.player_blue.play(board)
-        self.assertEqual(board, expected_board)
+        self.assertEqual(self.strategy_two.return_column(board), 6)
 
-    def test_player_with_strategy_two_red_not_winning_on_column(self):
+    def test_strategy_two_on_second_column(self):
         board = GameBoard.from_matrix([
                     ['red',    None,   None,   None,    None,   None,   None],
                     ['blue',   None,   None,   None,    None,   None,   None],
@@ -53,30 +36,4 @@ class TestPlayer(unittest.TestCase):
                     ['blue', 'blue',  'red',  'red',   'red', 'blue', 'blue'],
                     ['red',  'blue',  'red',  'red',  'blue', 'blue', 'blue'],
                     ['blue',  'red', 'blue',  'blue', 'blue',  'red',  'red']])
-        expected_board = GameBoard.from_matrix([
-                    ['red',    None,   None,   None,    None,   None,   None],
-                    ['blue',   None,   None,   None,    None,   None,   None],
-                    ['blue',  'red',  'red',  'red',  'blue', 'blue', 'blue'],
-                    ['blue', 'blue',  'red',  'red',   'red', 'blue', 'blue'],
-                    ['red',  'blue',  'red',  'red',  'blue', 'blue', 'blue'],
-                    ['blue',  'red', 'blue',  'blue', 'blue',  'red',  'red']])
-        self.player_red.play(board)
-        self.assertEqual(board, expected_board)
-
-    def test_player_with_strategy_two_red_not_winning_on_row(self):
-        board = GameBoard.from_matrix([
-                    [None,    None,   None,   None,   None,   None,   None],
-                    [None,    None,   None,  'red',  'red',  'red',   None],
-                    [None,    None,  'red',  'red',  'red', 'blue', 'blue'],
-                    [None,  'blue',  'red', 'blue',  'blue', 'red', 'blue'],
-                    ['red', 'blue',  'red', 'blue',  'red', 'blue', 'blue'],
-                    ['blue', 'red', 'blue',  'red',  'red',  'red',  'red']])
-        expected_board = GameBoard.from_matrix([
-                    [None,    None,   None,   None,   None,   None,   None],
-                    [None,    None,   None,  'red',  'red',  'red',   None],
-                    [None,    None,  'red',  'red',  'red', 'blue', 'blue'],
-                    ['red', 'blue',  'red', 'blue',  'blue', 'red', 'blue'],
-                    ['red', 'blue',  'red', 'blue',  'red', 'blue', 'blue'],
-                    ['blue', 'red', 'blue',  'red',  'red',  'red',  'red']])
-        self.player_red.play(board)
-        self.assertEqual(board, expected_board)
+        self.assertEqual(self.strategy_two.return_column(board), 2)
