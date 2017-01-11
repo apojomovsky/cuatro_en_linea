1c0192659faedeadc1c958aa554cb4ecc22aeaed
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-01-10 21:27:54 -0300

    First working version of strategy_three

--- game/strategy_three.py
+++ game/strategy_three.py
@@ -3,21 +3,30 @@ from game.gameboard import GameBoard
 
 class StrategyThree(Strategy):
     """Look for the column that is closest to win in a vertical
-       line for a given color, as long as a four-in-a-row could be completed
+       line for a given color, as long as a four-in-a-row could be completed.
+       Returns first non full column otherwise
     """
+    def _can_complete_four(self, board, column_index):
+        same_color_on_top = board.count_same_color_on_top(column_index, self._color)
+        current_free_entries = board.count_free_entries_on_column(column_index)
+        if same_color_on_top + current_free_entries >= 4:
+            return True
+        return False
 
     def _get_column_closest_to_win(self, board):
-        current_max = [0,0]
+        current_max = [0, 0]
         for column_index in range(GameBoard.COLUMNSCOUNT, 0, -1):
-            current_column_count = board.count_same_color_on_top(column_index, self._color)
-            current_free_entries = board.count_free_entries_on_column(column_index)
-            if current_column_count > current_max[1] and current_column_count + current_free_entries >= 4:
-                current_max[0] = column_index
-                current_max[1] = current_column_count
+            if self._can_complete_four(board, column_index):
+                same_color_on_top = board.count_same_color_on_top(column_index, self._color)
+                if same_color_on_top > current_max[1]:
+                    current_max[0] = column_index
+                    current_max[1] = same_color_on_top
         if current_max[1] > 0:
             return current_max[0]
-        else:
-            return 1
+        return False
 
     def return_column(self, board):
-        return self._get_column_closest_to_win(board)
+        closest = self._get_column_closest_to_win(board)
+        if closest:
+            return closest
+        return board.retrieve_first_non_full_column()
