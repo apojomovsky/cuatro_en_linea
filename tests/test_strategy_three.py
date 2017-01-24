33d25dccc56a4ef9c085cb9701ae2ff33f7de7e0
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-01-23 21:42:30 -0300

    Added comments on tests for strategy_three

--- tests/test_strategy_three.py
+++ tests/test_strategy_three.py
@@ -9,27 +9,39 @@ class TestPlayer(unittest.TestCase):
         self.strategy_three = StrategyThree()
         self.color = 'blue'
 
+    def test_strategy_three_choses_leftmost_column_from_empty_board(self):
+        board = GameBoard()
+        self.assertEqual(1, self.strategy_three.return_column(board, self.color))
+
     def test_strategy_three_choses_column_closest_to_win_for_blue(self):
+        """return_column will chose column three because is the closest
+           column to win for color blue"""
         board = GameBoard.from_matrix([
                     [None,  None,   None, None, None, None, None],
                     [None,  None,   None, None, None, None, None],
                     [None,  None,   None, None, None, None, None],
-                    ['red', None, 'blue', None, None, None, None],
+                    ['red', None,   None, None, None, None, None],
                     ['red', None, 'blue', None, None, None, None],
                     ['red', None, 'blue', None, None, None, None]])
         self.assertEqual(self.strategy_three.return_column(board, self.color), 3)
 
     def test_strategy_three_choses_sixth_column_because_unable_to_win_on_leftmost(self):
+        """return_column will chose column six because even while the first one is
+           suppossed to be closer to win, it can't because of lack of free entries
+        """
         board = GameBoard.from_matrix([
-                    [None,   None, None, None, None, None,    None],
-                    ['blue', None, None, None, None, None,    None],
-                    ['blue', None, None, None, None, None,    None],
-                    ['red',  None, None, None, None, None,    None],
-                    ['red',  None, None, None, None, 'blue',  None],
-                    ['red',  None, None, None, None, 'blue', 'blue']])
+                    [None,   None, None, None, None,  None,  None],
+                    ['blue', None, None, None, None,  None,  None],
+                    ['blue', None, None, None, None,  None,  None],
+                    ['red',  None, None, None, None,  None,  None],
+                    ['red',  None, None, None, None,  None,  None],
+                    ['red',  None, None, None, None, 'blue', None]])
         self.assertEqual(6, self.strategy_three.return_column(board, self.color))
 
-    def test_strategy_three_choses_leftmost_when_more_than_one_evaluates_equal(self):
+    def test_strategy_three_choses_leftmost_column_when_more_than_one_are_equal(self):
+        """return_column will chose column three because, even while it and
+           column four have the same ammount of blue chips, leftmost is priorized
+        """
         board = GameBoard.from_matrix([
                     [None,   None,   None,   None,  None, None,  None],
                     [None,   None,   None,   None,  None, None,  None],
@@ -39,11 +51,10 @@ class TestPlayer(unittest.TestCase):
                     ['red', 'red', 'blue', 'blue', 'red', 'red', None]])
         self.assertEqual(self.strategy_three.return_column(board, self.color), 3)
 
-    def test_strategy_three_choses_leftmost_column_from_empty_board(self):
-        board = GameBoard()
-        self.assertEqual(1, self.strategy_three.return_column(board, self.color))
-
     def test_strategy_three_choses_first_non_full_column_when_unable_to_win(self):
+        """If all the columns are unable to make a four-in-a-row, it will
+           return the first non full column.
+        """
         board = GameBoard.from_matrix([
                     [None,     None,   None,   None,   None,   None,   None],
                     ['red',   'red', 'blue',  'red',  'red',  'red', 'blue'],
