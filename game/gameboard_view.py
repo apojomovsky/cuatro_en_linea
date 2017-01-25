278206f5c0baf977c1bacf9ceb75a86a8669971d
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-01-24 22:30:42 -0300

    Created run_test.py file

--- game/gameboard_view.py
+++ game/gameboard_view.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-from game.gameboard import GameBoard
+from gameboard import GameBoard
 
 class GameBoardView(object):
 
     def show(self, board):
         """Print the board of a given gameboard in a nice format"""
+        print(chr(27) + "[2J")
         for row in board.retrieve_matrix():
             for entry in row:
                 print '{:4}'.format(entry),
--- game/run_game.py
+++ game/run_game.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
+import argparse
+import time
+import sys
 from match import Match
 from gameboard import GameBoard
 from gameboard_view import GameBoardView
 from player import Player
 from strategy_one import StrategyOne
 from strategy_two import StrategyTwo
-import time
-import sys
 
+lookup_strategies = {
+    'strategy_one': StrategyOne,
+    'strategy_two': StrategyTwo,
+#    'strategy_three': StrategyThree
+}
 
-if __name__ == "__main__":
-    player_one = Player('blue', StrategyOne())
-    player_two = Player('red', StrategyTwo())
+def run(player_one, player_two):
+    player_one = Player('blue', lookup_strategies.get(player_one)())
+    player_two = Player('red', lookup_strategies.get(player_two)())
     board = GameBoard()
+    view = GameBoardView()
     match = Match(player_one, player_two, board)
     while not match.is_over():
-        print(chr(27) + "[2J")
         match.play_next_turn()
-        board.show()
+        view.show(board)
         time.sleep(1)
     print "Color {} won!".format(match.who_won().color())
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Runs a new match of the Connect Four game')
+    parser.add_argument('-p1', '--player-one', action='store', dest='player_one',
+                        help="Strategy to be adopted by player one")
+    parser.add_argument('-p2', '--player-two', action='store', dest='player_two',
+                        help="Strategy to be adopted by player two")
+    args = parser.parse_args()
+    if not args.player_one or not args.player_two:
+        print "You must provide player_one and player_two strategies as arguments"
+        sys.exit(1)
+    run(args.player_one, args.player_two)
