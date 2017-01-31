9ce2829c6c6e8802c63d5ab0d9a66f61f8ccf077
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-01-31 00:34:38 -0300

    Added docstrings to test

--- tests/test_strategy_one.py
+++ tests/test_strategy_one.py
@@ -4,7 +4,7 @@ from game.gameboard import GameBoard
 from game.gameboard import BoardIsFull
 from game.strategy_one import StrategyOne
 
-class TestPlayer(unittest.TestCase):
+class TestStrategyOne(unittest.TestCase):
     def setUp(self):
         self.strategy_one = StrategyOne()
         self.color = 'blue'
@@ -23,7 +23,33 @@ class TestPlayer(unittest.TestCase):
                     ['blue', None, None, None, None, None, None]])
         self.assertEqual(self.strategy_one.return_column(board), 1)
 
+    def test_strategy_one_choses_leftmost_column_on_empty_board(self):
+        """
+        Strategy_one will return the first column (leftmost)
+        when the board is emtpy
+        """
+        board = GameBoard()
+        self.assertEqual(self.strategy_one.return_column(board), 1)
+
+    def test_strategy_one_on_near_empty_board(self):
+        """
+        The strategy looks from left to right for the first non-full column.
+        The test expects the first column.
+        """
+        board = GameBoard.from_matrix([
+                    [None,   None, None, None, None, None, None],
+                    [None,   None, None, None, None, None, None],
+                    [None,   None, None, None, None, None, None],
+                    [None,   None, None, None, None, None, None],
+                    [None,   None, None, None, None, None, None],
+                    ['blue', None, None, None, None, None, None]])
+        self.assertEqual(self.strategy_one.return_column(board), 1)
+
     def test_strategy_one_on_near_full_column(self):
+        """
+        Even if the column is almost full, the strategy_one will choose it
+        if it's the leftmost non full
+        """
         board = GameBoard.from_matrix([
                     [None,   None, None, None, None, None, None],
                     ['blue', None, None, None, None, None, None],
@@ -34,6 +60,10 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.strategy_one.return_column(board), 1)
 
     def test_strategy_one_choses_leftmost_non_full_column(self):
+        """
+        strategy_one will choose column two, because it's the leftmost column
+        that is not full
+        """
         board = GameBoard.from_matrix([
                     ['red',  None, None, None, None, None, None],
                     ['blue', None, None, None, None, None, None],
@@ -43,7 +73,23 @@ class TestPlayer(unittest.TestCase):
                     ['blue', None, None, None, None, None, None]])
         self.assertEqual(self.strategy_one.return_column(board), 2)
 
+    def test_strategy_one_choses_the_only_non_full_column(self):
+        """Strategy_one choses correctly the only column that is not full on the
+           whole board
+        """
+        board = GameBoard.from_matrix([
+                    ['red',  'blue',  'red',  None,  'blue', 'blue', 'blue'],
+                    ['red',   'red', 'blue',  None,  'blue',  'red',  'red'],
+                    ['red',  'blue',  'red',  None,  'blue', 'blue', 'blue'],
+                    ['blue', 'blue',  'red', 'blue',  'red',  'red', 'blue'],
+                    ['red',  'blue',  'red', 'blue',  'red', 'blue',  'red'],
+                    ['blue',  'red', 'blue',  'red',  'red',  'red',  'red']])
+        self.assertEqual(self.strategy_one.return_column(board), 4)
+
     def test_strategy_one_raises_exception_on_full_board(self):
+        """If it turns that the board is full, but the return_column message from
+           the strategy is called, a BoardIsFull exception will be raised.
+        """
         board = GameBoard.from_matrix([
                     ['red',  'blue',  'red',  'red', 'blue', 'blue', 'blue'],
                     ['red',   'red', 'blue',  'red', 'blue',  'red',  'red'],
