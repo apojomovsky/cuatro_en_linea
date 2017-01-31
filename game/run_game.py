4dbbcaa623473f8ed37ad84279a7396d8b195234
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-01-30 23:43:46 -0300

    Added rate argument to run_game

--- game/run_game.py
+++ game/run_game.py
@@ -15,7 +15,7 @@ lookup_strategies = {
 #    'strategy_three': StrategyThree
 }
 
-def run(player_one, player_two):
+def run(player_one, player_two, rate):
     player_one = Player('blue', lookup_strategies.get(player_one)())
     player_two = Player('red', lookup_strategies.get(player_two)())
     board = GameBoard()
@@ -23,8 +23,8 @@ def run(player_one, player_two):
     match = Match(player_one, player_two, board)
     while not match.is_over():
         match.play_next_turn()
-        view.show(board)
-        time.sleep(1)
+        view.show()
+        time.sleep(rate)
     print "Color {} won!".format(match.who_won().color())
 
 if __name__ == "__main__":
@@ -33,8 +33,10 @@ if __name__ == "__main__":
                         help="Strategy to be adopted by player one")
     parser.add_argument('-p2', '--player-two', action='store', dest='player_two',
                         help="Strategy to be adopted by player two")
+    parser.add_argument('-r', '--rate', action='store', dest='rate',
+                        default=2, help="Rate in seconds at which the game will be played")
     args = parser.parse_args()
     if not args.player_one or not args.player_two:
         print "You must provide player_one and player_two strategies as arguments"
         sys.exit(1)
-    run(args.player_one, args.player_two)
+    run(args.player_one, args.player_two, float(args.rate))
