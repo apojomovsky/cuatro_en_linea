217fd45c670dcbd96978251021a0765a8a568eef
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-02-11 15:46:41 -0300

    Fixed specific version syntax on setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(name='cuatro_en_linea',
       url='https://github.com/apojomovsky/cuatro_en_linea',
       license='Apache',
       install_requires=[
-          'mock-2.0.0',
+          'mock==2.0.0',
           'tabulate',
-          'numpy-1.12.0',
-          'scipy-0.18.1',
+          'numpy==1.12.0',
+          'scipy==0.18.1',
       ],
 zip_safe=False)
