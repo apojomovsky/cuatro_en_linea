a2ca04126860759144629f6f9c6159fff0385dd7
Andres Fortier <andres@creativa77.com>
2017-02-20 20:11:26 -0300

    Post-merge fix

--- game/random_column_strategy.py
+++ game/random_column_strategy.py
@@ -2,5 +2,6 @@ from strategy import Strategy
 import random
 
 class RandomColumnStrategy(Strategy):
-    def return_column(self, board, color):
+
+    def return_column(self, board):
         return random.choice(board.playable_positions())
